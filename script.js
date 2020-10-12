180dd26ed92021ffa97804641dd1f4955ecb29d5
Amalie Lemke <amalielemke@Amalies-MacBook-Pro-2.local>
2020-10-12 17:40:33 +0200

    farve skift

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ navToggleBtn = document.querySelector('.nav-toggle-btn');
 navToggleBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-  elNav.classList.toggle("nav-20");
-  elContent.classList.toggle("content-80");
+    elNav.classList.toggle("nav-20");
+    elContent.classList.toggle("content-80");
 }
 
 //
@@ -24,12 +24,33 @@ function toggleMenu() {
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.create({
-  trigger: ".product-2-trigger",
-  start: "top center",
-  end: "bottom center",
-  toggleClass: {
-    targets: "#produkt",
-    className: "produkt3",
-  },
-  markers: true,
+    trigger: ".product-2-trigger",
+    start: "top center",
+    //    end: "bottom center",
+    toggleClass: {
+        targets: "#produkt",
+        className: "produkt2",
+    },
+});
+
+
+ScrollTrigger.create({
+    trigger: ".product-3-trigger",
+    start: "top center",
+    //    end: "bottom center",
+    toggleClass: {
+        targets: "#produkt",
+        className: "produkt3",
+    },
+});
+
+
+ScrollTrigger.create({
+    trigger: ".product-4-trigger",
+    start: "top center",
+    //    end: "bottom center",
+    toggleClass: {
+        targets: "#produkt",
+        className: "produkt4",
+    }
 });
