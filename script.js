070fb31f64839d4124b0b42edf0a4e1233762f1a
Amalie Lemke <amalielemke@Amalies-MacBook-Pro-2.local>
2020-10-12 12:19:17 +0200

    n

--- script.js
+++ script.js
@@ -8,20 +8,3 @@ ScrollTrigger.create({
     toggleClass: "active2",
     markers: true,
 });
-
-
-
-
-
-
-//gsap.to(".ball-2", {
-//    backgroundColor: "red",
-//    ease: "elastic.out(1, 0.3)",
-//    scrollTrigger: {
-//        trigger: ".ball-2",
-//        markers: true,
-//        start: "top center",
-//        end: "bottom 25%",
-//        toggleActions: "play pause resume reset"
-//    }
-//});
